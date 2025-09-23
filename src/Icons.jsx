22cd704bbad2759a4e6ea9c19731ac31fcc5774d
Francis Villanueva <goosey816@gmail.com>
2025-09-23 18:16:02 +0800

    templating the whole site

---
+++ src/Icons.jsx
@@ -0,0 +1,12 @@
+function Icons(){
+    return(
+        <div id="icons">
+            <ul>About</ul>
+            <ul>Projects</ul>
+            <ul>Experience</ul>
+            <ul>Contact</ul>
+        </div>
+    );
+}
+
+export default Icons
