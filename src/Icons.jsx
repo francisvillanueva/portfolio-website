5823fd4872d7a7bcd00b8450a467446f6526f420
unknown <goosey816@gmail.com>
2026-01-30 23:33:32 +0800

    icons added

--- src/Icons.jsx
+++ src/Icons.jsx
@@ -1,11 +1,18 @@
+import DocumentsIcon from './assets/icons/Documents.webp';
+import ExplorerIcon from './assets/icons/Explorer-outline.webp';
+import InfoIcon from './assets/icons/Info.webp';
+import MailIcon from './assets/icons/Mail.webp';
+import NotepadIcon from './assets/icons/Notepad.webp';
+
 function Icons(){
     return(
         <div id="icons">
             <ul>
-                <li><a className="link" href=""><img src="" alt="" /> About</a></li>
-                <li><a className="link" href=""><img src="" alt="" /> Projects</a></li>
-                <li><a className="link" href=""><img src="" alt="" /> Experience</a></li>
-                <li><a className="link" href=""><img src="" alt="" /> Contact</a></li>
+                <li><a className="link" href=""><img src= {InfoIcon} alt="" />About</a></li>
+                <li><a className="link" href=""><img src={ExplorerIcon} alt="" />Links</a></li>
+                <li><a className="link" href=""><img src={DocumentsIcon} alt="" />Projects</a></li>
+                <li><a className="link" href=""><img src={NotepadIcon} alt="" />Resume</a></li>
+                <li><a className="link" href=""><img src={MailIcon} alt="" />Contact</a></li>
             </ul>
         </div>
     );
